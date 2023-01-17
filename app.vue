9c6f551a3db4d05b1e8df28a33caf0a69a6382b8
zeri1254 <72800353+zeri1254@users.noreply.github.com>
2023-01-18 01:51:31 +0500

    first commit

---
+++ app.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="{'dark': darkMode}">
+    
+    <div class='min-h-full'>
+
+      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8
+      lg:gap-5">
+
+      <!-- Left sidebar -->
+
+      <div class="border-2 border-red-500 md:block xs-col-span-1 xl:col-span-2">
+        <div class="sticky top-0">
+          <SidebarLeft />
+        </div>
+      </div>
+
+      <!-- Main Content -->
+
+      <!-- Right sidebar -->
+
+      </div>
+    
+    </div>
+
+    
+
+
+  </div>
+</template>
+
+<script setup>
+  const darkMode = ref(false)
+</script>
---
+++ components/Sidebar/Left.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+    <div class="w-8 h-8">
+        <LogoTwitter />
+    </div>
+    
+</div>  
+</template>
+
+<script setup>
+
+</script>
