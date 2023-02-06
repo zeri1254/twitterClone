5176f711d6dd4eb60620ff4f454f86517d210ce0
zeri1254 <72800353+zeri1254@users.noreply.github.com>
2023-02-06 16:57:28 +0500

    сделана менюшка под левый навбар

--- components/Sidebar/Left/Tab.vue
+++ components/Sidebar/Left/Tab.vue
@@ -1,14 +1,24 @@
 <template>
-    <nuxt-link to="#" class="flex items-center p-3 w-min hover:bg-gray-200" :class="defaultTransition">
+    <nuxt-link to="#" class="flex items-center p-3 w-min hover:bg-gray-200 rounded-full dark:hover:bg-dim-200 dark:text-white"
+    :class="defaultTransition">
         <div class="w-6 h-6 text-dark">
             <slot name="icon"></slot>
         </div>
 
-        <div>
-            <slot name="name"></slot>
+        <div class="hidden ml-4 text-xl xl:block" :class = "textClasses">
+        <slot name="name"></slot>
         </div>
     </nuxt-link>
 </template>
 <script setup>
 const {defaultTransition} = useTailwindConfig()
+
+const props = defineProps({
+    active: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const textClasses = computed(() => props.active ? 'font-semibold': 'font-normal')
 </script>
--- components/Sidebar/Left/index.vue
+++ components/Sidebar/Left/index.vue
@@ -10,15 +10,101 @@
         </nuxt-link>
     </div>
 
-    <SidebarLeftTab>
-        <template v-slot:icon>
-            <HomeIcon />
-        </template>
-        <template v-slot:name>
-            Home
-        </template>
-    </SidebarLeftTab>
-    
+    <div class="mt-2 space-y-3">
+
+        <SidebarLeftTab active>
+            <template v-slot:icon>
+                <HomeIcon />
+            </template>
+            <template v-slot:name>
+                Home
+            </template>
+        </SidebarLeftTab>
+
+        <SidebarLeftTab>
+            <template v-slot:icon>
+                <HashtagIcon />
+            </template>
+            <template v-slot:name>
+                Explore
+            </template>
+        </SidebarLeftTab>
+
+
+        <SidebarLeftTab>
+            <template v-slot:icon>
+                <BellIcon />
+            </template>
+            <template v-slot:name>
+                Notifications
+            </template>
+        </SidebarLeftTab>
+
+
+        <SidebarLeftTab>
+            <template v-slot:icon>
+                <InboxIcon />
+            </template>
+            <template v-slot:name>
+                Messages
+            </template>
+        </SidebarLeftTab>
+
+        <SidebarLeftTab>
+            <template v-slot:icon>
+                <BookmarkIcon />
+            </template>
+            <template v-slot:name>
+                Bookmarks
+            </template>
+        </SidebarLeftTab>
+
+
+        <SidebarLeftTab>
+            <template v-slot:icon>
+                <DocumentTextIcon />
+            </template>
+            <template v-slot:name>
+                Lists
+            </template>
+        </SidebarLeftTab>
+
+        <SidebarLeftTab>
+            <template v-slot:icon>
+                <UserIcon />
+            </template>
+            <template v-slot:name>
+                Profile
+            </template>
+        </SidebarLeftTab>
+
+        <SidebarLeftTab>
+            <template v-slot:icon>
+                <DotsCircleHorizontalIcon />
+            </template>
+            <template v-slot:name>
+                More
+            </template>
+        </SidebarLeftTab>
+
+        <div class="hidden xl:block">
+                <UIButton liquid size="lg" @on-click="emits('onTweet')">
+                    <span class="font-bold">
+                        Tweet
+                    </span>
+                </UIButton>
+            </div>
+
+            <div class="block xl:hidden">
+                <UIButton @on-click="emits('onTweet')">
+                    <div class="w-6 h-6 font-bold">
+                        <PencilIcon />
+                    </div>
+                </UIButton>
+            </div>
+    </div>
+
+
 </div>  
 </template>
 
