7250405fdec9e03cd1d90dfd51019ff8c89801ec
zeri1254 <72800353+zeri1254@users.noreply.github.com>
2023-02-11 22:56:35 +0500

    создание компонентов для правого сайдбара

---
+++ components/Sidebar/Right/PreviewCard/item.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="p-3 hover:bg-gray-100 cursor-pointer dark:hover:bg-dim-350 border-b" :class="wrapperClasses">
+        Item
+    </div>
+</template>
+<script setup>
+
+const { defaultTransition, twitterBorderColor } = useTailwindConfig()
+
+const wrapperClasses = computed(() => `${defaultTransition} ${twitterBorderColor}`)
+
+</script>
--- components/Sidebar/Right/index.vue
+++ components/Sidebar/Right/index.vue
@@ -1,5 +1,14 @@
 <template>
-    sidebarRight
+    <div class="flex flex-col">
+
+    <!-- Preview card: What's happening -->
+    <SidebarRightPreviewCard title="What's happening" />
+
+        <SidebarRightPreviewCardItem></SidebarRightPreviewCardItem>
+
+    <!-- Preview card: Who to follow -->
+    <SidebarRightPreviewCard title="Who to follow" />
+    </div>
 </template>
 <script setup>
 
